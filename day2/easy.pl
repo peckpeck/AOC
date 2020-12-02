1f9781cfe7dc24d32120348b0711173e038e790f
Benoît Peccatte <benoit.peccatte@normation.com>
2020-12-02 18:23:25 +0100

    Day 2 part 2

--- day2/easy.pl
+++ day2/easy.pl
@@ -9,5 +9,10 @@ print "$count\n";
 
 
 # One liner version : 
-# perl -ne '{/(\d+)-(\d+) (\w): (.*)/;$count++ if $4 =~ /^[^${3}]*(${3}[^${3}]*){$1,$2}[^${3}]*$/}END{print "$count\n";}' < input.txt
+# 1-
+# perl -ne '{/(\d+)-(\d+) (\w): (.*)/;$c++ if $4 =~ /^[^$3]*($3[^$3]*){$1,$2}[^$3]*$/}END{print "$c\n";}' < input.txt
+# 2-
+# perl -ne '{/(\d+)-(\d+) (\w): (.*)/;$a=$1-1;$b=$2-$1-1;$c++ if $4 =~ /^.{$a}$3.{$b}[^$3]|^.{$a}[^$3].{$b}$3/}END{print "$c\n";}' < input.txt
+
+
 
--- day2/rust/src/main.rs
+++ day2/rust/src/main.rs
@@ -5,21 +5,22 @@ use nom::character::complete::*;
 use nom::bytes::complete::*;
 
 #[allow(dead_code)]
-fn main() {
+fn main_1() {
     let mut file = File::open("input.txt").unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
     let mut count = 0;
     for line in contents.split("\n") {
         if line == "" { break; }
-        if parse(line).unwrap().1 {
+        if parse_1(line).unwrap().1 {
             count += 1;
         }
     }
     println!("{}",count);
 }
 
-fn parse(i: &str) -> IResult<&str, bool> {
+#[allow(dead_code)]
+fn parse_1(i: &str) -> IResult<&str, bool> {
     let (i, min) = digit1(i)?;
     let (i, _) = tag("-")(i)?;
     let (i, max) = digit1(i)?;
@@ -36,5 +37,38 @@ fn parse(i: &str) -> IResult<&str, bool> {
         if c == chr { count += 1; }
     }
     return Ok((i, count >=min && count <= max));
+}
+
+#[allow(dead_code)]
+fn main() {
+    let mut file = File::open("input.txt").unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let mut count = 0;
+    for line in contents.split("\n") {
+        if line == "" { break; }
+        if parse(line).unwrap().1 {
+            count += 1;
+        }
+    }
+    println!("{}",count);
+}
+
+#[allow(dead_code)]
+fn parse(i: &str) -> IResult<&str, bool> {
+    let (i, min) = digit1(i)?;
+    let (i, _) = tag("-")(i)?;
+    let (i, max) = digit1(i)?;
+    let (i, _) = tag(" ")(i)?;
+    let (i, chr) = anychar(i)?;
+    let (i, _) = tag(": ")(i)?;
+    let (i, st) = alphanumeric1(i)?;
+    
+    let min = min.parse::<usize>().unwrap()-1;
+    let max = max.parse::<usize>().unwrap()-1;
+    let st = st.as_bytes();
+    let chr = chr as u8;
+
+    return Ok((i, (st[min] != chr && st[max] == chr) || (st[min] == chr && st[max] != chr) ));
 }
 
